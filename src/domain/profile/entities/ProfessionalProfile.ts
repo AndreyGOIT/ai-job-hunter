b0aa6333d8525cea6d70f13182a920174aaf276b
Andrey Erokhin <andreyerokhin@MacBook-Air-Andrey.local>
2026-08-10 13:13:17 +0300

    feat: extend professional profile preferences

--- src/domain/profile/entities/ProfessionalProfile.ts
+++ src/domain/profile/entities/ProfessionalProfile.ts
@@ -1,11 +1,17 @@
 import { DesiredPosition } from "../value-objects/DesiredPosition";
+import { EmploymentType } from "../value-objects/EmploymentType";
+import { LocationPreference } from "../value-objects/LocationPreference";
 import { ProfileId } from "../value-objects/ProfileId";
 import { Skills } from "../value-objects/Skills";
+import { WorkMode } from "../value-objects/WorkMode";
 
 export type ProfessionalProfileProps = {
   id: ProfileId;
   desiredPosition: DesiredPosition;
   skills: Skills;
+  locationPreference: LocationPreference;
+  workMode: WorkMode;
+  employmentType: EmploymentType;
 };
 
 export class ProfessionalProfile {
@@ -25,6 +31,18 @@ export class ProfessionalProfile {
 
   public get skills(): Skills {
     return this.props.skills;
+  }
+
+  public get locationPreference(): LocationPreference {
+    return this.props.locationPreference;
+  }
+
+  public get workMode(): WorkMode {
+    return this.props.workMode;
+  }
+
+  public get employmentType(): EmploymentType {
+    return this.props.employmentType;
   }
 
   public equals(other: ProfessionalProfile): boolean {
